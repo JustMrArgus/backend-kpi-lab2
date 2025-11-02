07aa0a6829570b16e3bb46548d1e3a1d9a3975da
JustMrArgus <argusprogramming@protonmail.com>
2025-11-02 22:55:03 +0200

    Add routes to express app

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ const express = require("express");
 
 const healthcheckRoutes = require("./routes/healthcheck.route");
 const userRoutes = require("./routes/user.route");
+const categoryRoutes = require("./routes/category.route");
+const recordRoutes = require("./routes/record.route");
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/healthcheck", healthcheckRoutes);
+
 app.use("/users", userRoutes);
+app.use("/categories", categoryRoutes);
+app.use("/records", recordRoutes);
 
 module.exports = app;
